af088b9a5d220687b88cc64442eb0d64ba8fb453
fschn90 <hello@fschn.org>
2024-02-27 11:10:55 +0100

    more readme and conectp structure

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # GPE analyser
 
 identifying Geopolitical Entities in text
+
+## to-do / leads / idea collection
+
+geopy
+
+- https://stackoverflow.com/questions/1280266/how-to-classify-users-into-different-countries-based-on-the-location-field
+- https://stackoverflow.com/questions/4844811/how-can-i-determine-a-region-country-and-continent-based-on-a-city-using-pytho?rq=3
+- https://github.com/geopy/geopy
+- https://nominatim.org
+- https://www.geonames.org
--- recognizer.py
+++ recognizer.py
@@ -1,15 +1,17 @@
 import json
 import spacy
 
+## get data, sql call for articles not recognised yet 
+
 data = []
 with open('test_articles.json') as articles:
     opened = json.load(articles)
     for i in opened:
         data.append(i['story'])
 
-# nlp = spacy.load("de_core_news_sm")
-nlp = spacy.load("de_core_news_lg")
 
+## recognise gpes
+nlp = spacy.load("de_core_news_lg")
 for i in data:
     doc = nlp(i)
     for ent in doc.ents:
@@ -17,14 +19,10 @@ for i in data:
             print(ent.text, ent.label_)
 
 
-# nlp = spacy.load("de_core_news_lg")
-# doc = nlp(data[1])
-# for ent in doc.ents:
-#     print(ent.text, ent.start_char, ent.end_char, ent.label_)
-#
+## dump gpes into database per url
 
-# for i in data:
-#     doc = nlp(i)
-#     for token in doc:
-#         if token.ent_type_ == "LOC":
-#             print(token.text, token.ent_type_)
+## dump gpes into database per gpe
+
+## count ukraine
+
+## enrich gpe data 
