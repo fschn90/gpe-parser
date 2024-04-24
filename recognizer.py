9a2da0c25a6f5386f9bbe0ede0dfaca4c18e8ac2
flo-schn <hello@fschn.org>
2024-04-24 19:05:41 +0000

    last bug fixes and reorganisation

--- recognizer.py
+++ recognizer.py
@@ -4,9 +4,51 @@ import json
 import pymysql
 from dotenv import load_dotenv
 import os
-from loggi import classyLogStats
+import datetime
 
-class recognizer(classyLogStats):
+class classyLogStats():
+
+    def __init__(self):
+        load_dotenv('.env')
+        self.logStats = {}
+        self.logStats['start_time'] = datetime.datetime.now()
+        self.logStats['job'] = 'gpeCounter'
+        self.logStats['articlesAnalysed/orf'] = 0
+        self.logStats['articlesAnalysed/derstandard'] = 0
+        self.logStats['articlesAnalysed/oe24'] = 0
+        self.logStats['articlesAnalysed/krone'] = 0
+        self.logStats['articlesAnalysed/vol'] = 0
+        self.logStats['gpesCounted/orf'] = 0
+        self.logStats['gpesCounted/derstandard'] = 0
+        self.logStats['gpesCounted/oe24'] = 0
+        self.logStats['gpesCounted/krone'] = 0
+        self.logStats['gpesCounted/vol'] = 0 
+
+    def transformingLogDump(self):
+        self.logStats['finish_time'] = datetime.datetime.now()
+        self.logStats['elapsed_time'] = self.logStats['finish_time'] - self.logStats['start_time']
+        self.logStats = {key:val for key, val in self.logStats.items() if val != 0}
+        self.stt = json.dumps(self.logStats, sort_keys=True, default=str)
+        try:
+            dbconnection = pymysql.connect(
+                        host=os.environ.get("NMprod_db_domain"),
+                        user=os.environ.get("dbuser"),
+                        password=os.environ.get("dbpass"),
+                        db=os.environ.get("dbnameAna"),
+                        charset=os.environ.get("dbCharst"),
+                        cursorclass=pymysql.cursors.DictCursor,
+                    )
+            cursor = dbconnection.cursor()
+            cursor.execute(f"INSERT INTO {os.environ.get('dbnameAna')}.{os.environ.get('mainLogAna')} (logStats, finishTime) VALUES (%s, %s)", [self.stt, self.logStats['finish_time']])
+            dbconnection.commit()
+        except pymysql.Error as e:
+            print(e)         
+        finally:
+            dbconnection.close()
+            print(self.logStats)
+
+
+class gpeRecognizer(classyLogStats):
 
     def __init__(self):
         classyLogStats.__init__(self)
@@ -37,8 +79,8 @@ class recognizer(classyLogStats):
         nlp = spacy.load("de_core_news_lg")
         for result in self.results:
             data =[]
-            # logStats[f'articlesAnalysed/{result["paper"]}'] += 1
             self.logStats[f'articlesAnalysed/{result["paper"]}'] += 1
+            self.logStats['articlesAnalysed'] += 1
             filtered = {k: v for k, v in result.items() if v is not None}
             result.clear()
             result.update(filtered)
@@ -47,8 +89,8 @@ class recognizer(classyLogStats):
                 if ent.label_ == "LOC":
                     data.append(ent.text)
             countedGpes = Counter(data)
-            # logStats[f'gpesCounted/{result["paper"]}'] += sum(countedGpes.values())
             self.logStats[f'gpesCounted/{result["paper"]}'] += sum(countedGpes.values())
+            self.logStats['gpesCounted'] += sum(countedGpes.values())
             jsonGpes = json.dumps(countedGpes, sort_keys=True, default=str, ensure_ascii=False)
             self.parsed_data.append({'link':f"{result['link']}", 'paper':result['paper'], 'author':result.get('author', None), 'gpe':jsonGpes, 'scrapeDate':result.get('scrapeDate', None)})
 
@@ -83,8 +125,3 @@ class recognizer(classyLogStats):
             self.transformingLogDump()
         finally:
             dbconnection.close()
-
-razer = recognizer()
-razer.parsing()
-razer.dumping()
-razer.transformingLogDump()
